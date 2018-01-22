3c7114077250294960a249c65732349b3064cf76
shanchuang.zhang@hypervideo.cn <zhangshanchuang>
2018-01-22 16:17:19 +0800

    测试

--- src/main/java/com/nilo/web/WebInfo.java
+++ src/main/java/com/nilo/web/WebInfo.java
@@ -5,4 +5,33 @@ public class WebInfo {
 	/**
 	 * java web 基础知识
 	 */
+	
+	/**
+	 * 深入web请求过程
+	 *  1、http协议解析
+	 *  2、浏览器缓存机制
+	 *    Cache-Control/Pragma
+	 *    Expires 设置超期时间
+	 *    Last-Modified/Etag
+	 *  3、DNS域名解析：将URL中的域名解析成IP地址
+	 *    1）检查浏览器缓存是否存在对应的IP
+	 *    2）操作系统缓存：hosts文件  ；Linux配置 /etc/named.conf
+	 *    3）网络配置中的地址查询
+	 *    4）Root Server域名服务器
+	 *    ....
+	 *   4、CDN工作机制：内容分布式网络
+	 *   
+	 * 深入分析JavaIO的工作模式
+	 * 
+	 * 深入分析Javaweb 中的中文编码问题
+	 * 
+	 * Javac 编译原理
+	 * 
+	 * 深入class文件结构
+	 * 
+	 * 防止表单重复提交
+	 * 
+	 * Tomcat总体设计
+	 * 
+	 */
 }
